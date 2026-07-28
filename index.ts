fcd8aeb73d217ca46ba7abc2fbffe7e562a286c1
ctrlcat0x <sss.lata82@gmail.com>
2026-07-28 22:30:26 +0530

    fix: expose direct Hono Vercel entrypoint

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
-import app from "./apps/server/src/routes/index.js";
+import { Hono } from "hono";
+import routes from "./apps/server/src/routes/index.js";
+
+const app = new Hono();
+app.route("/", routes);
 
 export { app };
 export default app;
