943bfd1dbc49033a7240dbe54ceed8c1cd532fd6
ctrlcat0x <sss.lata82@gmail.com>
2026-07-28 22:49:08 +0530

    fix: export Vercel request handler

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import app from "../apps/server/src/routes/index.js";
 
-export default {
-  fetch(request: Request): Promise<Response> | Response {
-    return app.fetch(request);
-  },
-};
+export default function handler(
+  request: Request,
+): Promise<Response> | Response {
+  return app.fetch(request);
+}
